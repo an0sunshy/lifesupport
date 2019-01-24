5cd869bfc30b2b5a69fca76642363c1a681cbc11
Xiao Cheng <xiao@Xiaos-MacBook-Pro.local>
2019-01-24 17:46:27 -0500

    add gitignore

--- softlink.sh
+++ softlink.sh
@@ -6,7 +6,7 @@ NVIM_CONFIG_DIR=".config/nvim"
 for f in $(ls config); do
     TARGET="$HOME/.$f"
     if [ $f == "init.vim" ]; then
-        mkdir --parent $HOME/$NVIM_CONFIG_DIR || true
+        mkdir -p $HOME/$NVIM_CONFIG_DIR || true
         TARGET=$HOME/$NVIM_CONFIG_DIR/$f
     fi
     rm -f $TARGET
