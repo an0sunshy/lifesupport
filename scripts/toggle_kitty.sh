76bead7308c0da892b7d095bc971557cb1bfe1f0
an0sunshy <an0sunshy@gmail.com>
2021-05-18 00:19:29 -0500

    replace lctrl with ctrl for skhdrc

--- scripts/toggle_kitty.sh
+++ scripts/toggle_kitty.sh
@@ -1,16 +1,10 @@
 #!/usr/bin/env bash
 
-if ! pgrep -f "kitty" > /dev/null 2>&1; then
-    open -a "/Applications/kitty.app"
+script='
+if frontmost of application "kitty" then
+    tell application "System Events" to tell process "kitty" to set visible to not (get visible)
 else
-    script='
-    tell application "System Events" to set isVisible to visible of application process "kitty"
-
-    if isVisible then
-        tell application "System Events" to tell process "kitty" to set visible to not (get visible)
-    else
-        do shell script "open -a /Applications/kitty.app"
-    end if
-    '
-    osascript -e "${script}" > /dev/null 2>&1
-fi
+    do shell script "open -a /Applications/kitty.app"
+end if
+'
+osascript -e "${script}" > /dev/null 2>&1
